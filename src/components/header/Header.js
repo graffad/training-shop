9e2b4ab38ef7db4ed20a177a4270c72532f97ce3
Alexey Shabashev <54629615+graffad@users.noreply.github.com>
2022-02-19 16:45:00 +0300

    burger-menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
 import { ReactComponent as Phone } from "./icons/phone.svg";
 import { ReactComponent as Map } from "./icons/map.svg";
 import { ReactComponent as Time } from "./icons/time.svg";
@@ -13,65 +14,134 @@ import { ReactComponent as CartSvg } from "./icons/cart.svg";
 import { headerNav } from "../constants/constants";
 
 export default function Header() {
-  return (
-    <header className="header" data-test-id="header">
-      <section className="header-contacts-outer">
-        <div className="container">
-          <div className="header-contacts-inner">
-            <div className="header-contacts-inner__item">
-              <Phone />
-              +375 29 100 20 30
-            </div>
-            <div className="header-contacts-inner__item">
-              <Map />
-              Belarus, Gomel, Lange 17
-            </div>
-            <div className="header-contacts-inner__item">
-              <Time />
-              All week 24/7
-            </div>
+  const [burgerIsActive, setBurgerIsActive] = useState(false);
+  const headerRef = useRef(0);
+  const [burgerMargin, setBurgerMargin] = useState(90);
 
-            <div className="header-contacts-inner__item social-icons">
-              <Facebook className="social-icons__item" />
-              <Twitter className="social-icons__item" />
-              <Instagram className="social-icons__item" />
-              <Pinterest className="social-icons__item" />
-            </div>
-          </div>
-        </div>
-      </section>
-      <div className="container">
-        <section className="header-menu-wrapper">
-          <div className="header-logo">
-            <Link to="/" data-test-id="header-logo-link">
-              CleverShop
-            </Link>
-          </div>
-          <nav className="header-nav" data-test-id="menu">
-            {headerNav.map((item) => (
-              <NavLink
-                key={item.id}
-                to={`/${item.path}`}
-                data-test-id={`menu-link-${item.path}`}
-                className={({ isActive }) =>
-                  isActive ? "header-nav__item--active" : "header-nav__item"
-                }
-              >
-                {item.name}
-              </NavLink>
-            ))}
-          </nav>
-          <div className="header-controls">
-            <SearchSvg className="header-controls__item" />
-            <GlobusSvg className="header-controls__item" />
-            <ProfileSvg className="header-controls__item" />
-            <div className="header-controls__item">
-              <CartSvg />
-              <div className="header-cart-counter">2</div>
+  // if too many links and overflow
+  function onResize() {
+    setBurgerMargin(headerRef.current.offsetHeight);
+  }
+  // hide burger by click over
+  function onClickOver(el) {
+    if (!el.className.split(" ").some((cl) => /burger*/.test(cl))) {
+      setBurgerIsActive(false);
+      document.body.classList.remove("fixed-body");
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    document.addEventListener("click", (event) => {
+      onClickOver(event.target);
+    });
+    return () => {
+      window.removeEventListener("resize", onResize);
+      document.removeEventListener("click", (event) => {
+        onClickOver(event.target);
+      });
+    };
+  }, []);
+
+  return (
+    <>
+      <header ref={headerRef} className="header" data-test-id="header">
+        <section className="header-contacts-outer">
+          <div className="container">
+            <div className="header-contacts-inner">
+              <div className="header-contacts-inner__item">
+                <Phone />
+                +375 29 100 20 30
+              </div>
+              <div className="header-contacts-inner__item">
+                <Map />
+                Belarus, Gomel, Lange 17
+              </div>
+              <div className="header-contacts-inner__item">
+                <Time />
+                All week 24/7
+              </div>
+
+              <div className="header-contacts-inner__item social-icons">
+                <Facebook className="social-icons__item" />
+                <Twitter className="social-icons__item" />
+                <Instagram className="social-icons__item" />
+                <Pinterest className="social-icons__item" />
+              </div>
             </div>
           </div>
         </section>
+        <div className="container">
+          <section className="header-menu-wrapper">
+            <div className="header-logo">
+              <Link to="/" data-test-id="header-logo-link">
+                CleverShop
+              </Link>
+            </div>
+            <nav className="header-nav" data-test-id="menu">
+              {headerNav.map((item) => (
+                <NavLink
+                  key={item.id}
+                  to={`/${item.path}`}
+                  data-test-id={`menu-link-${item.path}`}
+                  className={({ isActive }) =>
+                    isActive ? "header-nav__item--active" : "header-nav__item"
+                  }
+                >
+                  {item.name}
+                </NavLink>
+              ))}
+            </nav>
+            <div className="header-controls">
+              <SearchSvg className="header-controls__item" />
+              <GlobusSvg className="header-controls__item" />
+              <ProfileSvg className="header-controls__item" />
+              <div className="header-controls__item">
+                <CartSvg />
+                <div className="header-cart-counter">2</div>
+              </div>
+              <button
+                onClick={() => {
+                  setBurgerIsActive(!burgerIsActive);
+                  document.body.classList.toggle("fixed-body");
+                }}
+                type="button"
+                data-test-id="burger-menu-btn"
+                className={`header-controls__item burger-btn ${
+                  burgerIsActive ? "burger-btn--active" : ""
+                }`}
+              >
+                <span className="burger-btn__icon" />
+              </button>
+            </div>
+          </section>
+        </div>
+      </header>
+
+      <div
+        // calc header height and marin burger
+        style={{
+          top: `${burgerMargin}px`,
+        }}
+        className={`burger-menu ${burgerIsActive ? "burger-menu--active" : ""}`}
+        data-test-id="burger-menu"
+      >
+        <div className="burger-menu-nav" data-test-id="menu">
+          {headerNav.map((item) => (
+            <NavLink
+              key={item.id}
+              to={`/${item.path}`}
+              data-test-id={`menu-link-${item.path}`}
+              className={({ isActive }) =>
+                isActive
+                  ? "burger-menu-nav__item--active"
+                  : "burger-menu-nav__item"
+              }
+            >
+              {item.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
-    </header>
+    </>
   );
 }
